889c32680d60869150cf51b9824e84d984795409
Kevin Aguilera <kevin.lovo@gmail.com>
2021-09-05 21:22:09 -0500

    cambios personales

---
+++ README.md
@@ -0,0 +1,13 @@
+## Portfolio Website For Developers 💯
+
+- Que tal este es mi portfolio web, el cual esta basado en una plantilla openSource y adaptada a mis necesidades.
+- Si gustas usarla eres libre de hacer y puedes o no mencionar a Many Arora quien es el autor orginal de dicha template.
+- Para este portfolio se utilizan tecnologias como Next.js y Tailwind. Pronto se estara haciendo el form de contacto funcional pero de momento no hace nada.
+- Si gustas usarlo solo necesitas clonar el repositorio y hacer lo cambios que necesites dentro de data.
+
+
+
+# License
+
+This template is completely open source and free to use. Use it for client projects or your own portfolio project. Give me credits at the footer (If you wish, it'll help me a lot :)).
+Creditos a: Manu Arora y FreeCodeCamp
---
+++ constants/data.js
@@ -0,0 +1,55 @@
+const userData = {
+  githubUsername: "KevinDanae",
+  name: "Kevin Aguilera",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.png",
+  email: "kevin.danaea@gmail.com",
+  phone: "+52 5620618927",
+  address: "Mexico, Mexico.",
+  projects: [
+    {
+      title: "Pokedex by Kevin",
+      link: "#",
+      imgUrl: "/pokedex.jpg",
+    },
+    {
+      title: "Rick and Morty App",
+      link: "https://nervous-wozniak-7cc5d3.netlify.app/",
+      imgUrl: "/rick.png",
+    },
+    {
+      title: "Weather App",
+      link: "https://loving-bhaskara-6cca62.netlify.app/",
+      imgUrl: "/weather.png",
+    },
+  ],
+  about: {
+    title:
+      "Soy Kevin, soy desarrollador web, me encanta comenzar nuevos proyectos y afrontar cualquier tipo de reto o dificultad en el camino",
+    description: [
+      `Decidi estudiar desarrollo web durante la pandemia de 2020, siempre me gusto la tecnologia y decidi empreder, me encontraba estudiando sistemas perdo tuve que salirme debido a la misma pandemia`,
+      `Comence estudiando las bases como HTML y CSS, una vez manejando decidi aprender Javascript y seguir estudiando, aprendi junto con esto Tailwind que fue uno de los frameworks de css que mas me gusto.`,
+      `Al principio todo era dificil para mi pero poco a poco y despues de estudiar bases y logica de programacion, entre a Henry un bootcamp donde termine de reafirmar todo mi conocimiento, ahora manejo React, Redux, Node, Express y me encuentro aprendiendo VUE y Golang.`,
+    ],
+  },
+  experience: [
+    {
+      title: "Henry proyecto grupal",
+      company: "Henry",
+      year: "2021",
+      companyLink: "https://www.soyhenry.com/",
+      desc: "Desarrollo de un proyecto full stack, utilizando SCRUM como metodologia agil.",
+    }
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1iumR1XFbLWrwBHrc9GpugBL5MwvOojc9/view?usp=sharing",
+  socialLinks: {
+    instagram: "#",
+    twitter: "#",
+    linkedin: "https://www.linkedin.com/in/kevin-danae/",
+    github: "https://github.com/KevinDanae",
+    facebook: "#",
+  },
+};
+
+export default userData;
